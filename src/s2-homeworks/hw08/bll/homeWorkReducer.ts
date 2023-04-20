5c5256318fd420ad7497f70f8ae3f2e5c263df79
Endy Guff <andreymaranchak@yandex.ru>
2023-04-20 16:08:21 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,36 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            let newState = [...state]
+            if (action.payload === 'up'){
+                newState = state.sort((a, b)=>{
+                    if (a.name>b.name){
+                        return 1
+                    }
+                    if (a.name<b.name){
+                        return - 1
+                    }
+                    return 0
+                })
+            }
+            if (action.payload === 'down'){
+                newState = state.sort((a, b)=>{
+                    if (a.name<b.name){
+                        return 1
+                    }
+                    if (a.name>b.name){
+                        return - 1
+                    }
+                    return 0
+                })
+            }
+            return newState // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(el=>el.age>action.payload) // need to fix
         }
         default:
             return state
