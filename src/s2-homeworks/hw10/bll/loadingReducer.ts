e6ed80f7acf6c74a2c5d40a0c533c697da6a475c
Endy Guff <andreymaranchak@yandex.ru>
2023-05-06 13:10:47 +0300

    hw10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,10 +2,15 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+type InitStateType = {
+    isLoading: boolean
+}
+
+export const loadingReducer = (state = initState, action: LoadingActionType): InitStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case "CHANGE_LOADING":
+            return (state.isLoading)?{...state, isLoading: false}:{...state, isLoading: true}
         default:
             return state
     }
